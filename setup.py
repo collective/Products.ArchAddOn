0b3621f8ee586bfe4252360515e72940f076a253
maurits <maurits@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-01 12:12:24 +0000

    Set version to 1.6dev in version.txt, clean up setup.py, use README.txt from main package.

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+versionfile = open(os.path.join('Products', 'ArchAddOn', 'version.txt'))
+version = versionfile.read().strip()
+versionfile.close()
+
+readmefile = open(os.path.join('Products', 'ArchAddOn', 'README.txt'))
+readme = readmefile.read().strip()
+readmefile.close()
+
+historyfile = open(os.path.join('Products', 'ArchAddOn', 'HISTORY.txt'))
+history = historyfile.read().strip()
+historyfile.close()
+
+long_description = readme + "\n\n" + history
 
 setup(name='Products.ArchAddOn',
       version=version,
       description="Straightforward toolbox of field types, widgets, and validators for Archetypes.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_description,
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
